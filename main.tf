5d802d6cc0fb5a8c76d5c4add8acab248a99cbb5
Marcos Souza <marcos.souza@iteris.com.br>
2023-12-06 15:21:07 -0300

    Add template rendering and output
    This commit adds a template rendering feature to the main.tf file. It uses the `template_file` data source to render a simple template that prints the content of a variable. The rendered template is then outputted as `rendered_template`.

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-# Create a null resource for testing
-resource "null_resource" "atlantis_hw1" {
-  
+# render a simple template that prints the content of a variable
+
+data "template_file" "workspace" {
+  template = "${var.workspace}"
+}
+
+output "rendered_template" {
+  value = data.template_file.workspace.rendered
+  description = "The rendered workspace template"
 }
